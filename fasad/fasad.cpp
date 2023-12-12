97bed1271eebb2a39f8aea7d6e709ee30194d163
vadim <vadim@XCSENON>
2023-12-12 16:17:55 +0200

    Добавьте файлы проекта.

---
+++ fasad/fasad.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+// Subsystem A
+class SubsystemA {
+public:
+    void operationA() const {
+        std::cout << "Subsystem A operation" << std::endl;
+    }
+};
+
+// Subsystem B
+class SubsystemB {
+public:
+    void operationB() const {
+        std::cout << "Subsystem B operation" << std::endl;
+    }
+};
+
+// Subsystem C
+class SubsystemC {
+public:
+    void operationC() const {
+        std::cout << "Subsystem C operation" << std::endl;
+    }
+};
+
+// Facade
+class Facade {
+private:
+    SubsystemA subsystemA;
+    SubsystemB subsystemB;
+    SubsystemC subsystemC;
+
+public:
+    void operation() const {
+        std::cout << "Facade operation" << std::endl;
+        subsystemA.operationA();
+        subsystemB.operationB();
+        subsystemC.operationC();
+    }
+};
+
+int main() {
+    Facade facade;
+    facade.operation();
+
+    return 0;
+}
